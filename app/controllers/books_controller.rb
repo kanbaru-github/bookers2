0a6143ff5b9598839c73c3e8dc8dc112359d0fe3
kanbaru-github <usupfv@gmail.com>
2021-06-21 08:03:41 +0000

    completion

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,16 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
-    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path, notice: 'successfully!'
+
+      redirect_to book_path(@book), notice: 'successfully!'
     else
       @books = Book.all
       @user = current_user
@@ -25,6 +26,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user.id != current_user.id
+      redirect_to books_path
+    end
   end
 
   def update
