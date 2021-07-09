f22809e3d3494ec2fe3caee904e10d9393adf0ae
kanbaru-github <usupfv@gmail.com>
2021-07-09 15:42:34 +0000

    first_commit

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,24 +3,26 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   # 認証ユーザーのみにアクセスを許可する
 
-	def create
-	  current_user.follow(params[:user_id])
-		redirect_to request.referer
-		# 遷移前のページのURL取得
-	end
+  def create
+  	current_user.follow(params[:user_id])
+  	# userテーブルからidを受け取るため
+  	redirect_to request.referer
+  	# 遷移前のページのURL取得
+  end
 
   def destroy
-		current_user.unfollow(params[:user_id])
+  	current_user.unfollow(params[:user_id])
+		# userモデルで定義したものを受け取る
 		redirect_to request.referer
-	end
+  end
 
-	def followings
-		user = User.find(params[:user_id])
+  def followings
+  	user = User.find(params[:user_id])
 		@users = user.followings
   end
 
-	def followers
-		user = User.find(params[:user_id])
+  def followers
+  	user = User.find(params[:user_id])
 		@users = user.followers
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,8 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
-  attachment :profile_image
+  attachment :profile_image, destroy: false
+  # userモデルが消えてもrefileに残る
 
   validates :name, presence: true, uniqueness:true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
@@ -45,6 +46,7 @@ class User < ApplicationRecord
   # Userモデルにメソッドを記述
    # ユーザーをフォローする
   def follow(user_id)
+    # user_idは動作する際に一時的に作られる為schemaにはない
     relationships.create(followed_id: user_id)
   end
 
